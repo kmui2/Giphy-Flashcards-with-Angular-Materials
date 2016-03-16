5fcd23d3367087130487dcb93032337171b34abb
Jeremy Elbourn <jelbourn@google.com>
2016-03-16 16:55:35 -0700

    Showcase each component

--- src/app/material2-app.spec.ts
+++ src/app/material2-app.spec.ts
@@ -5,13 +5,12 @@ beforeEachProviders(() => [Material2AppApp]);
 
 describe('App: Material2App', () => {
   it('should have the `defaultMeaning` as 42', inject([Material2AppApp], (app: Material2AppApp) => {
-    expect(app.defaultMeaning).toBe(42);
+
   }));
 
   describe('#meaningOfLife', () => {
     it('should get the meaning of life', inject([Material2AppApp], (app: Material2AppApp) => {
-      expect(app.meaningOfLife()).toBe('The meaning of life is 42');
-      expect(app.meaningOfLife(22)).toBe('The meaning of life is 22');
+
     }));
   });
 });
--- src/app/material2-app.ts
+++ src/app/material2-app.ts
@@ -1,21 +1,35 @@
 import {Component} from 'angular2/core';
+import {MD_SIDENAV_DIRECTIVES} from '@angular2-material/sidenav';
+import {MdToolbar} from '@angular2-material/toolbar';
+import {MdButton} from '@angular2-material/button';
+import {MdCheckbox} from '@angular2-material/checkbox';
+import {MdRadioButton} from '@angular2-material/radio';
+import {MdRadioDispatcher} from '@angular2-material/radio/radio_dispatcher';
+import {MdSpinner} from '@angular2-material/progress-circle';
+import {MD_CARD_DIRECTIVES} from '@angular2-material/card';
 import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
 
 
 @Component({
   selector: 'material2-app-app',
-  providers: [],
+  providers: [MdRadioDispatcher],
   templateUrl: 'app/material2-app.html',
-  directives: [ROUTER_DIRECTIVES],
+  styleUrls: ['app/material2-app.css'],
+  directives: [
+    ROUTER_DIRECTIVES,
+    MD_SIDENAV_DIRECTIVES,
+    MD_CARD_DIRECTIVES,
+    MdToolbar,
+    MdButton,
+    MdCheckbox,
+    MdRadioButton,
+    MdSpinner,
+  ],
   pipes: []
 })
 @RouteConfig([
 
 ])
 export class Material2AppApp {
-  defaultMeaning: number = 42;
-  
-  meaningOfLife(meaning?: number) {
-    return `The meaning of life is ${meaning || this.defaultMeaning}`;
-  }
+
 }
