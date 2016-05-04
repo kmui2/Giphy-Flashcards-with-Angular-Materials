87734bf7be706ef209f7d68efd73ee6d4a2e8c09
Jeremy Elbourn <jelbourn@google.com>
2016-05-04 00:08:00 -0700

    Fix bad merge

--- src/client/app.ts
+++
@@ -1,4 +0,0 @@
-import {bootstrap} from 'angular2/platform/browser';
-import {Material2AppApp} from './app/material2-app';
-
-bootstrap(Material2AppApp, []);
--- src/client/app/material2-app.spec.ts
+++
@@ -1,8 +0,0 @@
-import {describe, it, expect, beforeEachProviders, inject} from 'angular2/testing';
-import {Material2AppApp} from '../app/material2-app';
-
-beforeEachProviders(() => [Material2AppApp]);
-
-describe('App: Material2App', () => {
-});
-
