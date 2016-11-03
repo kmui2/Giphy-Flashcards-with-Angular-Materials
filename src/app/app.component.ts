bc653a8eb6a4696356a05bf0474f49fe15126a1d
Jeremy Elbourn <jelbourn@google.com>
2016-11-03 16:35:34 -0700

    Update to alpha.10

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, Optional} from '@angular/core';
+import {MdDialog, MdDialogRef, MdSnackBar} from '@angular/material';
 
 
 @Component({
@@ -8,6 +9,7 @@ import {Component} from '@angular/core';
 })
 export class Material2AppAppComponent {
   isDarkTheme: boolean = false;
+  lastDialogResult: string;
 
   foods: any[] = [
     {name: 'Pizza', rating: 'Excellent'},
@@ -17,10 +19,39 @@ export class Material2AppAppComponent {
 
   progress: number = 0;
 
-  constructor() {
+  constructor(private _dialog: MdDialog, private _snackbar: MdSnackBar) {
     // Update the value for the progress-bar on an interval.
     setInterval(() => {
       this.progress = (this.progress + Math.floor(Math.random() * 4) + 1) % 100;
     }, 200);
   }
+
+  openDialog() {
+    let dialogRef = this._dialog.open(DialogContent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      this.lastDialogResult = result;
+    })
+  }
+
+  showSnackbar() {
+    this._snackbar.open('YUM SNACKS', 'CHEW');
+  }
+}
+
+
+@Component({
+  template: `
+    <p>This is a dialog</p>
+    <p>
+      <label>
+        This is a text box inside of a dialog.
+        <input #dialogInput>
+      </label>
+    </p>
+    <p> <button md-button (click)="dialogRef.close(dialogInput.value)">CLOSE</button> </p>
+  `,
+})
+export class DialogContent {
+  constructor(@Optional() public dialogRef: MdDialogRef<DialogContent>) { }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
 import {MaterialModule} from '@angular/material';
-import {Material2AppAppComponent} from './app.component';
+import {Material2AppAppComponent, DialogContent} from './app.component';
 
 @NgModule({
   imports: [
     BrowserModule,
     MaterialModule.forRoot(),
   ],
-  declarations: [Material2AppAppComponent],
+  declarations: [Material2AppAppComponent, DialogContent],
+  entryComponents: [DialogContent],
   bootstrap: [Material2AppAppComponent],
 })
 export class MaterialAppModule { }
