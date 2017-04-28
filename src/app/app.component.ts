edafaea38c6eb4c423365b0dc90667fedab4fd7e
m67hoff <mhoff@web.de>
2017-04-28 19:59:24 +0200

    update to material 2.0.0-beta.3 / angular 4.0.0
    Signed-off-by: m67hoff <mhoff@web.de>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,30 +1,42 @@
-import {Component, Optional} from '@angular/core';
-import {MdDialog, MdDialogRef, MdSnackBar} from '@angular/material';
+import { Component, Optional } from '@angular/core';
 
+import { MdDialog, MdDialogRef, MdSnackBar } from '@angular/material';
+import { OverlayContainer } from '@angular/material';
 
 @Component({
-  selector: 'material2-app-app',
-  templateUrl: 'app.component.html',
-  styleUrls: ['app.component.css'],
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
 })
-export class Material2AppAppComponent {
+export class AppComponent {
   isDarkTheme: boolean = false;
   lastDialogResult: string;
-
+  
   foods: any[] = [
-    {name: 'Pizza', rating: 'Excellent'},
-    {name: 'Burritos', rating: 'Great'},
-    {name: 'French fries', rating: 'Pretty good'},
+    { name: 'Pizza', rating: 'Excellent' },
+    { name: 'Burritos', rating: 'Great' },
+    { name: 'French fries', rating: 'Pretty good' },
   ];
 
   progress: number = 0;
 
-  constructor(private _dialog: MdDialog, private _snackbar: MdSnackBar) {
+  constructor(private _dialog: MdDialog, private _snackBar: MdSnackBar, private _overlayContainer: OverlayContainer) {
     // Update the value for the progress-bar on an interval.
     setInterval(() => {
       this.progress = (this.progress + Math.floor(Math.random() * 4) + 1) % 100;
     }, 200);
   }
+  
+ toggleTheme(){
+   this.isDarkTheme = !this.isDarkTheme;
+   
+   if (this.isDarkTheme){
+      this._overlayContainer.themeClass = 'app-dark-theme';
+   } else {
+      this._overlayContainer.themeClass = 'default';     
+   }
+   
+ }
 
   openDialog() {
     let dialogRef = this._dialog.open(DialogContent);
@@ -34,11 +46,12 @@ export class Material2AppAppComponent {
     })
   }
 
-  showSnackbar() {
-    this._snackbar.open('YUM SNACKS', 'CHEW');
+  openSnackBar() {
+    this._snackBar.open('YUM SNACKS', 'CHEW', { duration: 3000 });  
   }
 }
 
+// second Component 
 
 @Component({
   template: `
@@ -53,5 +66,5 @@ export class Material2AppAppComponent {
   `,
 })
 export class DialogContent {
-  constructor(@Optional() public dialogRef: MdDialogRef<DialogContent>) { }
+  constructor( @Optional() public dialogRef: MdDialogRef<DialogContent>) { }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,27 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {MaterialModule} from '@angular/material';
-import {Material2AppAppComponent, DialogContent} from './app.component';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import 'hammerjs';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
+
+import { AppComponent, DialogContent } from './app.component';
 
 @NgModule({
+  declarations: [
+    AppComponent,
+    DialogContent
+  ],
   imports: [
     BrowserModule,
-    MaterialModule.forRoot(),
+    FormsModule,
+    HttpModule,
+    MaterialModule,
+    BrowserAnimationsModule
   ],
-  declarations: [Material2AppAppComponent, DialogContent],
   entryComponents: [DialogContent],
-  bootstrap: [Material2AppAppComponent],
+  bootstrap: [AppComponent]
 })
-export class MaterialAppModule { }
+export class AppModule {}
